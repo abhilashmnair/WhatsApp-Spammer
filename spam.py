7a1711d42ad9a6b49017c1f2ad65f59f4960685c
Abhilash M Nair <abhilashmnair20@gmail.com>
2020-11-06 12:47:18 +0530

    Create spam.py

---
+++ spam.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+driver = webdriver.Chrome('chromedriver.exe')
+driver.implicitly_wait(15) 
+driver.get('https://web.whatsapp.com')
+victimName = input("Victim Name : ")
+spamText = input("Enter spam text : ")
+driver.find_element_by_css_selector("span[title='" + victimName + "']").click()
+while(True):
+    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div[1]/div[2]').send_keys(spamText)
+    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
